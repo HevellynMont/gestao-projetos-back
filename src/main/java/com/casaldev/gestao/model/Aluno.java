001e30dc18c2de6f728a2f27206a2cc71e377a10
HevellynMont <Hevellynmonteiro321@gmail.com>
2026-04-03 10:13:19 -0300

    adiciona a coluna matricula na tabela alunos

--- src/main/java/com/casaldev/gestao/model/Aluno.java
+++ src/main/java/com/casaldev/gestao/model/Aluno.java
@@ -1,17 +1,20 @@
 package com.casaldev.gestao.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
 @Entity
 @Table(name = "alunos")
 public class Aluno {
@@ -19,4 +22,6 @@ public class Aluno {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+    @Column(unique = true)
+    private String matricula;
 }
